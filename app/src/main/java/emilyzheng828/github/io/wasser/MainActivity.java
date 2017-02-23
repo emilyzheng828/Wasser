414eafafc7e51fd8dd8abc4dcfced38a8496d7cc
Ataul Munim <hello@ataulm.com>
2017-02-23 19:44:26 +0000

    Mainactivity added

---
+++ app/src/main/java/emilyzheng828/github/io/wasser/MainActivity.java
@@ -0,0 +1,13 @@
+package emilyzheng828.github.io.wasser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
